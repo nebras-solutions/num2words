4adf6a2a4de08eab73b5d0befd23f0089d0ae893
Slim BHIRI <slim@nebras.tn>
2017-12-05 04:06:32 +0100

    Don't have to do that.
    This reverts commit 8c32b5b

--- setup.py
+++ setup.py
@@ -18,18 +18,18 @@ LONG_DESC = open('README.rst', 'rt').read() + '\n\n' + \
 
 setup(
     name='num2words',
-    version='0.5.7',
+    version='0.5.6',
     description='Modules to convert numbers to words. Easily extensible.',
     long_description=LONG_DESC,
     license='LGPL',
     author='Taro Ogawa <tso at users sourceforge net>',
     author_email='tos@users.sourceforge.net',
-    maintainer='Nebras Solutions S.A.R.L.',
-    maintainer_email='slim@nebras.tn',
+    maintainer='Savoir-faire Linux inc.',
+    maintainer_email='ernesto.rodriguezortiz@savoirfairelinux.com',
     keywords=' number word numbers words convert conversion i18n '
              'localisation localization internationalisation '
              'internationalization',
-    url='https://github.com/nebras-solutions/num2words',
+    url='https://github.com/savoirfairelinux/num2words',
     packages=find_packages(exclude=['tests']),
     test_suite='tests',
     classifiers=CLASSIFIERS,
