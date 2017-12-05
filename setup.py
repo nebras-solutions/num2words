8c32b5b1069e6d86ae6bd356ba700fee4dc47b93
Slim BHIRI <slim@nebras.tn>
2017-12-05 03:38:43 +0100

    Update package informations.

--- setup.py
+++ setup.py
@@ -18,18 +18,18 @@ LONG_DESC = open('README.rst', 'rt').read() + '\n\n' + \
 
 setup(
     name='num2words',
-    version='0.5.6',
+    version='0.5.7',
     description='Modules to convert numbers to words. Easily extensible.',
     long_description=LONG_DESC,
     license='LGPL',
     author='Taro Ogawa <tso at users sourceforge net>',
     author_email='tos@users.sourceforge.net',
-    maintainer='Savoir-faire Linux inc.',
-    maintainer_email='ernesto.rodriguezortiz@savoirfairelinux.com',
+    maintainer='Nebras Solutions S.A.R.L.',
+    maintainer_email='slim@nebras.tn',
     keywords=' number word numbers words convert conversion i18n '
              'localisation localization internationalisation '
              'internationalization',
-    url='https://github.com/savoirfairelinux/num2words',
+    url='https://github.com/nebras-solutions/num2words',
     packages=find_packages(exclude=['tests']),
     test_suite='tests',
     classifiers=CLASSIFIERS,
